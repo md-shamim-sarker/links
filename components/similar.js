54e7cd4393d616c6184fc44db3f7b409ed5bcfb7
shamimspro <shamimspro@gmail.com>
2022-06-22 17:36:41 +0600

    components

---
+++ components/similar.js
@@ -0,0 +1,18 @@
+let menu = 
+'<a href="#">Typing</a>\
+<a href="#">Tutorial</a>\
+<a href="#">Youtube</a>\
+<a href="#">Facebook</a>';
+
+let footer = 
+'<p>&copy; All rights reserved | Md. Shamim Saker | 2022</p>\
+<div>\
+    <a href="#"><img src="./images/icons8-facebook-40.png" alt=""></a>\
+    <a href="#"><img src="./images/icons8-instagram-40.png" alt=""></a>\
+    <a href="#"><img src="./images/icons8-linkedin-40.png" alt=""></a>\
+    <a href="#"><img src="./images/icons8-twitter-40.png" alt=""></a>\
+    <a href="#"><img src="./images/icons8-youtube-40.png" alt=""></a>\
+</div>';
+
+document.getElementById('menu').innerHTML = menu;
+document.getElementById('footer').innerHTML = footer;
